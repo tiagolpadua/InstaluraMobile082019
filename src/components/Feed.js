7273368a4d2b1ec18a5dab38d9b18eeaeec5196e
Tiago Pádua <tiagolpadua@gmail.com>
2019-08-25 20:41:19 -0300

    c3.1

---
+++ src/components/Feed.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Dimensions, Image, Text, View} from 'react-native';
+
+const width = Dimensions.get('screen').width;
+export default class Feed extends Component {
+  render() {
+    return (
+      <View>
+        <Text>rafael</Text>
+        <Image
+          source={require('../../resources/img/alura.png')}
+          style={{width: width, height: width}}
+        />
+      </View>
+    );
+  }
+}
