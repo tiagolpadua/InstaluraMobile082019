fc0b73627b58b8f53bc232b02349779bdb3c7365
Tiago Pádua <tiagolpadua@gmail.com>
2019-09-01 23:03:28 -0300

    c9.1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {AppRegistry} from 'react-native';
-import Feed from './src/components/Feed';
 import {name as appName} from './app.json';
+import Login from './src/screens/Login';
 
-AppRegistry.registerComponent(appName, () => Feed);
+AppRegistry.registerComponent(appName, () => Login);
