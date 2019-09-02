dd7daaa23e2f7e6dc023d6a9c4db12e44c423261
Tiago Pádua <tiagolpadua@gmail.com>
2019-09-01 22:43:10 -0300

    c8

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -10,12 +10,31 @@ export default class Feed extends Component {
       fotos: [],
     };
   }
-
   componentDidMount() {
     fetch('https://instalura-api.herokuapp.com/api/public/fotos/rafael')
       .then(resposta => resposta.json())
       .then(json => this.setState({fotos: json}));
   }
+  like = idFoto => {
+    const foto = this.state.fotos.find(f => f.id === idFoto);
+    let novaLista = [];
+    if (!foto.likeada) {
+      novaLista = [...foto.likers, {login: 'meuUsuario'}];
+    } else {
+      novaLista = foto.likers.filter(liker => {
+        return liker.login !== 'meuUsuario';
+      });
+    }
+    const fotoAtualizada = {
+      ...foto,
+      likeada: !foto.likeada,
+      likers: novaLista,
+    };
+    const fotos = this.state.fotos.map(f =>
+      f.id === fotoAtualizada.id ? fotoAtualizada : f,
+    );
+    this.setState({fotos});
+  };
 
   render() {
     return (
@@ -23,11 +42,12 @@ export default class Feed extends Component {
         style={styles.container}
         keyExtractor={item => item.id + ''}
         data={this.state.fotos}
-        renderItem={({item}) => <Post foto={item} />}
+        renderItem={({item}) => <Post foto={item} likeCallback={this.like} />}
       />
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     marginTop: Platform.OS == 'ios' ? 20 : 0,
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,5 @@
 import React, {Component} from 'react';
-import {
-  Dimensions,
-  Image,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {Dimensions, Image, StyleSheet, Text, View} from 'react-native';
 import InputComentario from './InputComentario';
 import Likes from './Likes';
 
@@ -15,28 +8,7 @@ const width = Dimensions.get('screen').width;
 export default class Post extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      foto: this.props.foto,
-    };
   }
-
-  like = () => {
-    const {foto} = this.state;
-    let novaLista = [];
-    if (!foto.likeada) {
-      novaLista = [...foto.likers, {login: 'meuUsuario'}];
-    } else {
-      novaLista = foto.likers.filter(liker => {
-        return liker.login !== 'meuUsuario';
-      });
-    }
-    const fotoAtualizada = {
-      ...foto,
-      likeada: !foto.likeada,
-      likers: novaLista,
-    };
-    this.setState({foto: fotoAtualizada});
-  };
 
   exibeLegenda(foto) {
     if (foto.comentario === '') {
@@ -52,7 +24,9 @@ export default class Post extends Component {
   }
 
   adicionaComentario = (valorComentario, inputComentario) => {
-    if (valorComentario === '') return;
+    if (valorComentario === '') {
+      return;
+    }
     const novaLista = [
       ...this.state.foto.comentarios,
       {
@@ -70,7 +44,7 @@ export default class Post extends Component {
   };
 
   render() {
-    const {foto} = this.state;
+    const {foto, likeCallback} = this.props;
     return (
       <View>
         <View style={styles.cabecalho}>
@@ -79,7 +53,7 @@ export default class Post extends Component {
         </View>
         <Image source={{uri: foto.urlFoto}} style={styles.foto} />
         <View style={styles.rodape}>
-          <Likes foto={foto} likeCallback={this.like} />
+          <Likes foto={foto} likeCallback={likeCallback} />
           {this.exibeLegenda(foto)}
 
           {foto.comentarios.map(comentario => (
