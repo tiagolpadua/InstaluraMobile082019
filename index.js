749df0d0157dbef7fe681f09b231de19ad2ec97c
Tiago Pádua <tiagolpadua@gmail.com>
2019-09-02 22:02:51 -0300

    c10.1

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 import Login from './src/screens/Login';
 
-AppRegistry.registerComponent(appName, () => Login);
+const AppNavigator = createStackNavigator({
+  Login: {
+    screen: Login,
+  },
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+AppRegistry.registerComponent(appName, () => AppContainer);
