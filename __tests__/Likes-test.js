3e817c5d2d375b7420b97cfe297d4104939058f4
Tiago Pádua <tiagolpadua@gmail.com>
2019-10-23 22:15:22 -0300

    ap2.3

---
+++ __tests__/Likes-test.js
@@ -0,0 +1,10 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Likes from '../src/components/Likes';
+import foto from './foto.json';
+
+it('Deve renderizar corretamente', () => {
+  const likes = renderer.create(<Likes foto={foto} likeCallback={jest.fn} />);
+  expect(likes).toMatchSnapshot();
+});
